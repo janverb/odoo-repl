4f01f937a90220db5594e266a8d305f2d18da0c3
Jan Verbeek <jverbeek@therp.nl>
2019-11-25 11:05:05 +0100

    Split EnvAccess into EnvProxy and ModelProxy, add docstrings

--- odoo_repl/__init__.py
+++ odoo_repl/__init__.py
@@ -21,7 +21,9 @@ except ImportError:
 from datetime import datetime
 from functools import partial
 
-if sys.version_info >= (3, 0):
+PY3 = sys.version_info >= (3, 0)
+
+if PY3:
     unicode = str
 
 
@@ -57,26 +59,21 @@ def enable(env, module_name='__main__', color=True):
     __main__.browse = partial(browse, env)
     __main__.sql = partial(sql, env)
 
-    __main__.env = EnvAccess(env)
+    __main__.env = EnvProxy(env)
     __main__.u = UserBrowser(env)
     __main__.cfg = ConfigBrowser(env)
     __main__.ref = DataBrowser(env)
 
     for part in __main__.env._base_parts():
         if not hasattr(__main__, part) and not hasattr(builtins, part):
-            setattr(
-                __main__,
-                part,
-                EnvAccess(
-                    env, part, env[part] if part in env.registry else None
-                ),
-            )
+            setattr(__main__, part, ModelProxy(env, part))
 
     if not color:
         disable_color()
 
 
 def disable_color():
+    """Disable colored output for model and record summaries."""
     global red, green, yellow, blue, purple, cyan
     red = green = yellow = blue = purple = cyan = lambda s: s
 
@@ -109,8 +106,8 @@ def color_repr(owner, field_name):
     """Return a color-coded representation of an object."""
     try:
         obj = getattr(owner, field_name)
-    except Exception as e:
-        return red(str(e))
+    except Exception as err:
+        return red(repr(err))
     field_type = owner._fields[field_name].type
     if obj is False and field_type != 'boolean' or obj is None:
         return red(repr(obj))
@@ -169,7 +166,7 @@ def field_color(field):
 
 
 def _unwrap(obj):
-    if isinstance(obj, EnvAccess):
+    if isinstance(obj, ModelProxy):
         obj = obj._real
     if not _is_record(obj):
         raise TypeError
@@ -177,6 +174,7 @@ def _unwrap(obj):
 
 
 def odoo_model_summary(obj):
+    """Summarize a model's fields."""
     obj = _unwrap(obj)
 
     fields = sorted(obj._fields)
@@ -198,6 +196,7 @@ def odoo_model_summary(obj):
 
 
 def odoo_repr(obj):
+    """Display all of a record's fields."""
     obj = _unwrap(obj)
 
     if len(obj) > 3:
@@ -242,82 +241,122 @@ def _BaseModel_repr_pretty_(self, printer, cycle):
 
 
 def oprint(obj):
+    """Display all records in a set, even if there are a lot."""
     print('\n\n'.join(odoo_repr(record) for record in obj))
 
 
 def displayhook(obj):
-    if isinstance(obj, EnvAccess) and obj._real is not None:
-        if len(obj._real) == 0:
-            print(odoo_model_summary(obj._real))
-            builtins._ = obj
-            return
-        obj = obj._real
-    if _is_record(obj):
+    """A sys.displayhook replacement that pretty-prints models and records."""
+    if isinstance(obj, ModelProxy) and obj._real is not None:
+        print(odoo_model_summary(obj._real))
+        builtins._ = obj
+    elif _is_record(obj):
         print(odoo_repr(obj))
         builtins._ = obj
     else:
         sys.__displayhook__(obj)
 
 
-class EnvAccess(object):
-    def __init__(self, env, path='', real=None):
+class EnvProxy(object):
+    """A wrapper around an odoo.api.Environment object.
+
+    Models returned by indexing will be wrapped in a ModelProxy for nicer
+    behavior. Models can also be accessed as attributes, with tab completion.
+    """
+
+    def __init__(self, env):
         self._env = env
-        self._path = path
-        self._real = real
 
     def __getattr__(self, attr):
         if attr.startswith('__'):
             raise AttributeError
-        if not self._path and hasattr(self._env, attr):
+        if hasattr(self._env, attr):
             return getattr(self._env, attr)
-        new = self._path + '.' + attr if self._path else attr
-        if (
-            hasattr(self._real, attr)
-            and new not in self._env.registry
-            and not any(m.startswith(new + '.') for m in self._env.registry)
-        ):
-            return getattr(self._real, attr)
-        if new in self._env.registry:
-            return EnvAccess(self._env, new, self._env[new])
-        if any(m.startswith(new + '.') for m in self._env.registry):
-            return EnvAccess(self._env, new)
-        raise AttributeError("Model '{}' does not exist".format(new))
+        if attr in self._base_parts():
+            return ModelProxy(self._env, attr)
+        raise AttributeError
 
     def __dir__(self):
-        if not self._path:
-            return self._base_parts() + dir(self._env)
-        return dir(self._real) + list(
-            {
-                mod[len(self._path) + 1 :].split('.', 1)[0]
-                for mod in self._env.registry
-                if mod.startswith(self._path + '.')
-            }
+        listing = set(super().__dir__()) if PY3 else {'_base_parts'}
+        listing.update(self._base_parts())
+        listing.update(
+            attr for attr in dir(self._env) if not attr.startswith('__')
         )
+        return sorted(listing)
 
     def _base_parts(self):
         return list({mod.split('.', 1)[0] for mod in self._env.registry})
 
     def __repr__(self):
-        return "EnvAccess({!r}, {!r})".format(self._env, self._path)
+        return "{}({!r})".format(self.__class__.__name__, self._env)
 
     def __getitem__(self, ind):
-        if not self._path:
-            return EnvAccess(self._env, ind, self._env[ind])
-        self._ensure_real()
+        if ind not in self._env.registry:
+            raise IndexError("Model '{}' does not exist".format(ind))
+        return ModelProxy(self._env, ind)
+
+    def _ipython_key_completions_(self):
+        return self._env.registry.keys()
+
+
+class ModelProxy(object):
+    """A wrapper around an Odoo model.
+
+    Records can be browsed with indexing syntax, other models can be used
+    with tab-completed attribute access, there are added convenience methods,
+    and instead of an ordinary repr a summary of the fields is shown.
+    """
+
+    def __init__(self, env, path):
+        self._env = env
+        self._path = path
+        self._real = env[path] if path in env.registry else None
+
+    def __getattr__(self, attr):
+        if attr.startswith('__'):
+            raise AttributeError
+        new = self._path + '.' + attr
+        if new in self._env.registry:
+            return self.__class__(self._env, new)
+        if any(m.startswith(new + '.') for m in self._env.registry):
+            return self.__class__(self._env, new)
+        if self._real is None:
+            raise AttributeError("Model '{}' does not exist".format(new))
+        return getattr(self._real, attr)
+
+    def __dir__(self):
+        listing = set(super().__dir__()) if PY3 else set()
+        if self._real is not None:
+            listing.update(
+                attr for attr in dir(self._real) if not attr.startswith('__')
+            )
+        listing.update(
+            mod[len(self._path) + 1 :].split('.', 1)[0]
+            for mod in self._env.registry
+            if mod.startswith(self._path + '.')
+        )
+        return sorted(listing)
+
+    def __repr__(self):
+        return "<{}({})>".format(self.__class__.__name__, self._path)
+
+    def _repr_pretty_(self, printer, cycle):
+        if self._real is not None and printer.indentation == 0:
+            printer.text(odoo_model_summary(self._real))
+        else:
+            printer.text(repr(self))
+
+    def __getitem__(self, ind):
+        if self._real is None:
+            return IndexError("Model '{}' does not exist".format(self._path))
         if not ind:
             return self._real
-        if isinstance(ind, list):
+        if isinstance(ind, (list, set)):
             ind = tuple(ind)
-        # Odoo doesn't mind if you try to browse an id that doesn't exist
-        # We do mind and want to throw an exception as soon as possible
-        # There's a .exists() method for that
-        # But in Odoo 8 a non-existent record can end up in the cache and then
-        # some fields mysteriously break
-        # So to avoid that, check before browsing it
         if not isinstance(ind, tuple):
             ind = (ind,)
-        if not ind:
-            return self._real
+        # Browsing a non-existent record can cause weird caching problems, so
+        # check first
         real_ind = set(
             sql(
                 self._env,
@@ -327,27 +366,14 @@ class EnvAccess(object):
         )
         missing = set(ind) - real_ind
         if missing:
-            if len(missing) == 1:
-                raise ValueError("Record {} does not exist".format(*missing))
-            raise ValueError(
+            raise IndexError(
                 "Records {} do not exist".format(', '.join(map(str, missing)))
             )
         return self._real.browse(ind)
 
-    def _ipython_key_completions_(self):
-        if not self._path:
-            return self._env.registry.keys()
+    def _ensure_real(self):
         if self._real is None:
-            return []
-        # IPython doesn't seem to want to display int keys, at least in the
-        # versions I tested it, but this can't hurt
-        return self._all_ids_()
-
-    def _all_ids_(self):
-        self._ensure_real()
-        return sql(
-            self._env, 'SELECT id FROM {}'.format(self._env[self._path]._table)
-        )
+            raise TypeError("Model '{}' does not exist".format(self._path))
 
     def search(
         self, args=(), offset=0, limit=None, order='id', count=False, **kwargs
@@ -360,6 +386,7 @@ class EnvAccess(object):
         """
         self._ensure_real()
         args = list(args)
+        # TODO: inspect fields
         args.extend((k, '=', getattr(v, 'id', v)) for k, v in kwargs.items())
         return self._real.search(
             args, offset=offset, limit=limit, order=order, count=count
@@ -377,6 +404,7 @@ class EnvAccess(object):
                 and value
                 and _is_record(value[0])
             ):
+                # TODO: typecheck model
                 field_type = self._real._fields[key].type
                 if field_type.endswith('2many'):
                     kwargs[key] = [(4, record.id) for record in value]
@@ -388,26 +416,22 @@ class EnvAccess(object):
                     kwargs[key] = value.id
         return self._real.create(kwargs)
 
+    def _all_ids_(self):
+        """Get all record IDs in the database."""
+        self._ensure_real()
+        return sql(
+            self._env, 'SELECT id FROM {}'.format(self._env[self._path]._table)
+        )
+
     def _mod_(self):
         """Get the ir.model record of the model."""
         self._ensure_real()
         return self._env['ir.model'].search([('model', '=', self._path)])
 
-    def _shuf_(self, n=1):
+    def _shuf_(self, num=1):
         """Return a random record, or multiple."""
         self._ensure_real()
-        return self._real.browse(random.sample(self._all_ids_(), n))
-
-    def _repr_pretty_(self, printer, cycle):
-        """IPython pretty-printing."""
-        if self._real is not None and printer.indentation == 0:
-            printer.text(odoo_model_summary(self._real))
-        else:
-            printer.text(repr(self))
-
-    def _ensure_real(self):
-        if self._real is None:
-            raise TypeError("Model '{}' does not exist".format(self._path))
+        return self._real.browse(random.sample(self._all_ids_(), num))
 
 
 def sql(env, query, *args):
@@ -424,6 +448,8 @@ def sql(env, query, *args):
 
 def browse(env, url):
     """Take a browser form URL and figure out its record."""
+    # TODO: handle other views more intelligently
+    #       perhaps based on the user?
     query = urlparse.parse_qs(urlparse.urlparse(url).fragment)
     return env[query['model'][0]].browse(int(query['id'][0]))
 
@@ -447,11 +473,9 @@ class UserBrowser(object):
 
     def __getattr__(self, attr):
         # IPython does completions in a separate thread.
-        # Odoo doesn't like that very much. So completions on attributes of
-        # u fail.
-        # We can solve that some of the time by remembering things we've
-        # completed before.
-        # Another option in some cases might be to use direct SQL queries.
+        # Odoo doesn't like that. So completions on attributes of `u` fail.
+        # We can solve that sometimes by remembering things we've completed
+        # before.
         user = self._env['res.users'].search([('login', '=', attr)])
         if not user:
             raise AttributeError("User '{}' not found".format(attr))
@@ -499,6 +523,8 @@ class DataBrowser(object):
 
 
 class DataModuleBrowser(object):
+    """Access data records within a module. Created by DataBrowser."""
+
     def __init__(self, env, module):
         self._env = env
         self._module = module
@@ -520,6 +546,7 @@ class DataModuleBrowser(object):
 
 
 def find_data(env, obj):
+    """Find the ir.model.data record for a record or an ID."""
     ir_model_data = env['ir.model.data']
     if isinstance(obj, str):
         if '.' in obj:
@@ -533,6 +560,8 @@ def find_data(env, obj):
 
 
 def _is_record(obj):
+    """Return whether an object is an Odoo record."""
+    # This has to work without importing BaseModel
     return hasattr(obj, '_ids') and type(obj).__module__ in {
         'openerp.api',
         'odoo.api',
@@ -540,6 +569,8 @@ def _is_record(obj):
 
 
 class ConfigBrowser(object):
+    """Access ir.config.parameter entries as attributes."""
+
     def __init__(self, env, path=''):
         self._env = env
         self._path = path
@@ -547,7 +578,7 @@ class ConfigBrowser(object):
     def __repr__(self):
         real = self._env['ir.config_parameter'].get_param(self._path)
         if real is False:
-            return "ConfigBrowser({!r}, {!r})".format(self._env, self._path)
+            return "<{}({})>".format(self.__class__.__name__, self._path)
         return repr(real)
 
     def __str__(self):
@@ -570,9 +601,11 @@ class ConfigBrowser(object):
     def __dir__(self):
         if not self._path:
             return self._env['ir.config_parameter'].search([]).mapped('key')
-        results = (
-            self._env['ir.config_parameter']
-            .search([('key', '=like', self._path + '.%')])
-            .mapped('key')
+        return list(
+            {
+                result[len(self._path) + 1 :]
+                for result in self._env['ir.config_parameter']
+                .search([('key', '=like', self._path + '.%')])
+                .mapped('key')
+            }
         )
-        return list({result[len(self._path) + 1 :] for result in results})
