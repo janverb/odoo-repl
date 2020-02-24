f9bf60a699c4f100a747d4fe3a23c53c9227d47f
Jan Verbeek <jverbeek@therp.nl>
2020-02-24 15:26:40 +0100

    [FIX] Update fish completions

--- completions/odoo-repl.fish
+++ completions/odoo-repl.fish
@@ -1,10 +1,10 @@
 # Link/copy to ~/.config/fish/completions/odoo-repl.fish
 
-function __fish_complete_odoo_database
+function __odoo_repl_complete_odoo_database
     psql -AtqwlF \t 2>/dev/null | awk 'NF > 1 { print $1 }'
 end
 
-complete -c odoo-repl -s d -l database -a '(__fish_complete_odoo_database)' -x  -d "Database name"
+complete -c odoo-repl -s d -l database -a '(__odoo_repl_complete_odoo_database)' -x -d "Database name"
 complete -c odoo-repl -s h -l help -d "Show help"
 complete -c odoo-repl -s c -l command -x -d "Initial command to execute"
 complete -c odoo-repl -l ipython -d "Use IPython instead of the default REPL"
@@ -13,3 +13,4 @@ complete -c odoo-repl -l no-color -d "Disable colored output"
 complete -c odoo-repl -l bg-editor -d "Allow editors to run in the background"
 complete -c odoo-repl -l interpreter -r -d "Use another interpreter"
 complete -c odoo-repl -l no-interactive -d "Immediately quit odoo-repl after starting"
+complete -c odoo-repl -l run-tests -d "Run tests, then exit"
