d39b95e44bc681e1af6360d582fe089997301a7f
Jan Verbeek <jverbeek@therp.nl>
2021-05-05 16:56:17 +0200

    [IMP] Enable clickable URLs by default

--- odoo_repl/config.py
+++ odoo_repl/config.py
@@ -7,10 +7,17 @@ runtime.
 import os
 
 color = not (os.environ.get("NO_COLOR") or os.environ.get("ODOO_REPL_NO_COLOR"))
+
 editor = tuple((os.environ.get("EDITOR") or "nano").split())
 bg_editor = bool(os.environ.get("ODOO_REPL_BG_EDITOR"))
+
+# Clickable filenames could interfere with linkification that includes line numbers,
+# so they're turned off by default (I also didn't find myself using it)
 clickable_filenames = bool(os.environ.get("ODOO_REPL_CLICKABLE_FILENAMES"))
-clickable_records = bool(os.environ.get("ODOO_REPL_CLICKABLE_RECORDS"))
+clickable_records = not os.environ.get("ODOO_REPL_NO_CLICKABLE_RECORDS")
+
 grep = os.environ.get("ODOO_REPL_GREP", "")
+
 force_pdb = bool(os.environ.get("ODOO_REPL_FORCE_PDB"))
+
 slow_tests = bool(os.environ.get("ODOO_REPL_SLOW_TESTS"))
